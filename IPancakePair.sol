04f8bf92c3dc5603148ed334f734307b06b04bf4
umairb56 <83267668+umairb56@users.noreply.github.com>
2021-11-24 18:25:43 +0530

    Update IPancakePair.sol

--- IPancakePair.sol
+++ IPancakePair.sol
@@ -1,6 +1,4 @@
-pragma solidity >=0.6.6;
-
-interface IApePair {
+interface IPancakePair {
     event Approval(address indexed owner, address indexed spender, uint value);
     event Transfer(address indexed from, address indexed to, uint value);
 
@@ -49,4 +47,19 @@ interface IApePair {
     function sync() external;
 
     function initialize(address, address) external;
+}
+
+// a library for performing overflow-safe math, courtesy of DappHub (https://github.com/dapphub/ds-math)
+library SafeMath {
+    function add(uint x, uint y) internal pure returns (uint z) {
+        require((z = x + y) >= x, 'ds-math-add-overflow');
+    }
+
+    function sub(uint x, uint y) internal pure returns (uint z) {
+        require((z = x - y) <= x, 'ds-math-sub-underflow');
+    }
+
+    function mul(uint x, uint y) internal pure returns (uint z) {
+        require(y == 0 || (z = x * y) / y == x, 'ds-math-mul-overflow');
+    }
 }
