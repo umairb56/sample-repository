d1cfcaa7941a3ba24c13ef577d91a3f16d65f03f
umairb56 <83267668+umairb56@users.noreply.github.com>
2021-11-24 18:26:30 +0530

    Update IPancakePair.sol

--- IPancakePair.sol
+++ IPancakePair.sol
@@ -1,3 +1,4 @@
+pragma solidity =0.6.6;
 interface IPancakePair {
     event Approval(address indexed owner, address indexed spender, uint value);
     event Transfer(address indexed from, address indexed to, uint value);
